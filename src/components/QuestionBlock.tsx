c1052ec57de4b86424a370bb45cd7e3439ed59d7
Quintin Vizza <vizzaq@mail.sacredheart.edu>
2025-09-21 23:45:51 -0400

    added

--- src/components/QuestionBlock.tsx
+++ src/components/QuestionBlock.tsx
@@ -4,23 +4,32 @@ import { useState } from "react";
 import Coin from "./Coin";
 
 export default function QuestionBlock() {
+  const [used, setUsed] = useState(false);
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
-    if (!active) {
+    if (!used) {
       setActive(true);
-      setTimeout(() => setActive(false), 1200); // reset after animation
+      setTimeout(() => {
+        setActive(false);
+        setUsed(true); // turn into empty block
+      }, 1200); // animation length
     }
   };
 
   return (
     <div className="relative w-16 h-16">
-      {/* Question Block */}
+      {/* Block itself */}
       <div
         onClick={handleClick}
-        className={`w-16 h-16 bg-[url('/questionBlock.png')] bg-cover pixelated cursor-pointer transition-transform ${
+        className={`w-16 h-16 bg-cover pixelated cursor-pointer transition-transform ${
           active ? "-translate-y-2" : ""
         }`}
+        style={{
+          backgroundImage: used
+            ? "url('/emptyBlock.png')" // transparent empty block PNG
+            : "url('/questionBlock.png')", // transparent question block PNG
+        }}
       />
 
       {/* Coin animation */}
